85b8c091ab22235107c82cf03aa6b4462fb35474
luvlqq <lualgod92@gmail.com>
2023-01-29 17:19:39 +0300

    Routes Created a logger.service.ts, base.controller.ts, routes.interface.ts. add a controllers and loggers

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import express, {Express} from "express";
 import {userRouter} from "./users/users";
 import {Server} from 'http'
+import {LoggerService} from "./logger/logger.service";
 
 
 export class App {
-    app: Express
+    app: Express;
     port: number;
     server: Server;
-    constructor() {
+    logger: LoggerService;
+    constructor(logger: LoggerService) {
         this.app = express();
         this.port = 8000;
+        this.logger = logger;
     }
 
     useRoutes(){
@@ -18,7 +21,7 @@ export class App {
     public async init(){
         this.useRoutes();
         this.server = this.app.listen(this.port);
-        console.log(`Server has been started: http://localhost:${this.port}`);
+        this.logger.log(`Server has been started: http://localhost:${this.port}`);
     }
 }
 
