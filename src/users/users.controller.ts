03f4e61b8b3a44713bef9648898cb0efb83ce0a6
luvlqq <lualgod92@gmail.com>
2023-02-02 20:12:12 +0300

    user login create a user login method

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -25,13 +25,25 @@ export class UsersController extends BaseController implements IUserController {
 				func: this.register,
 				middlewares: [new ValidateMiddleware(UserRegisterDto)],
 			},
-			{ path: '/login', method: 'post', func: this.login },
+			{
+				path: '/login',
+				method: 'post',
+				func: this.login,
+				middlewares: [new ValidateMiddleware(UserLoginDto)],
+			},
 		]);
 	}
 
-	login(req: Request<{}, {}, UserLoginDto>, res: Response, next: NextFunction): void {
-		console.log(req.body);
-		next(new HTTPError(401, 'Auth Error'));
+	async login(
+		req: Request<{}, {}, UserLoginDto>,
+		res: Response,
+		next: NextFunction,
+	): Promise<void> {
+		const result = await this.userService.validateUser(req.body);
+		if (!result) {
+			return next(new HTTPError(401, 'Auth Error'));
+		}
+		this.ok(res, {});
 	}
 	async register(
 		{ body }: Request<{}, {}, UserRegisterDto>,
